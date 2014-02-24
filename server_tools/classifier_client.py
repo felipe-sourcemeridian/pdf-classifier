ef418a9de439c5ff4a159f41154efb941803c13d
root <root@linkapedia.linkapedia>
2014-02-24 13:23:33 -0500

    modified classifier client for handle new protocol for read server's response

--- server_tools/classifier_client.py
+++ server_tools/classifier_client.py
@@ -7,17 +7,10 @@ import os
 TITLE_PACKET=1
 DOCUMENT_PACKET=2
 END_PACKET=3
-def classify_multiples_directorys(host, port, directory):
-	thread_list=[]
-	for dir_path,dir_names,file_names in os.walk(directory):
-		for dir_name in dir_names:
-			print "init work in directory %s" % directory + dir_name
-			thread_work=threading.Thread(group = None,target = classify_directory, name = None, args = (host, port, directory + dir_name + "/"), kwargs = {})
-			thread_work.start()
-			thread_list.append(thread_work)
-		break
+DOCUMENT_RESPONSE_PACKET = 126
+END_DOCUMENT_RESPONSE = 127
 def classify_directory(directory, server):
-	file_classified = 0
+	files_classified = 0
 	for dirnames, dirs, files in os.walk(directory):
 		for file_name in files:
 			try:
@@ -26,6 +19,7 @@ def classify_directory(directory, server):
 				print "files classified %d"% files_classified
 			except Exception as e:
 				print e.message	
+
 def classify_document_on_server(document_name, server):
 	try:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,12 +30,17 @@ def classify_document_on_server(document_name, server):
 		print response
 	finally:
 		sock.close()
-def read_classifier_response_from_socket(sock):
-	print "reading content from socket"
-	fd = sock.makefile()
-	classifier_response = fd.read()
-	fd.close()
-	return classifier_response
+def read_classifier_response_from_socket(socket):
+	classifier_header = read_header_response_from_socket(socket)
+	document_response = ''
+	while classifier_header[1] != END_DOCUMENT_RESPONSE:
+		document_response += socket.recv(classifier_header[0])
+		classifier_header = read_header_response_from_socket(socket)
+	return document_response
+def read_header_response_from_socket(socket):	
+	header = socket.recv(8)
+	header = struct.unpack("<IB3s", header)
+	return (header[0], header[1])
 def read_document(document_name):
 	fd = open(document_name, "r")
 	content = fd.readlines()
@@ -59,8 +58,8 @@ def send_document_content_to_classify_to_server(document_content, sock):
 	sock.send(struct.pack("IB3s" ,0, END_PACKET, "car"))
 if __name__ == '__main__':
 	parse = argparse.ArgumentParser(description = 'Classifier Client')
-	parse.add_argument('--host', help="classifier's server host name", dest='host',required=True)
-	parse.add_argument('-p', '--port', type=int, help="classifier's server port", dest='port',required=True)
+	parse.add_argument('--host', help="classifier's server host name", dest='host', default='localhost')
+	parse.add_argument('-p', '--port', type=int, help="classifier's server port", dest='port', default=5000)
 	parse.add_argument('-f', '--file', help='file to classify', dest='file', required=False)
 	parse.add_argument('-d', '--directory',help='classify whole files on directory', dest='directory', required=False)
 	args=parse.parse_args()
