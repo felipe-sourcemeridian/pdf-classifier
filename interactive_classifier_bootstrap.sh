7b432c964018ad857e6292e90e0b401d9455fa72
root <root@carlos-VirtualBox.(none)>
2014-04-22 17:26:16 -0500

    fixed wrong path on script

--- interactive_classifier_bootstrap.sh
+++ interactive_classifier_bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget https://s3.amazonaws.com/classifier-process/data/interactive_classifier.tar.gz
-tar -xvzf classifier.tar.gz
+tar -xvzf interactive_classifier.tar.gz
 sudo ./install_classifier.sh $1
-rm classifier.tar.gz
-rm install_classifier.sh
-rm classifier.dbg
+rm -f interactive_classifier.tar.gz
+rm -f install_classifier.sh
+rm -f classifier.dbg
