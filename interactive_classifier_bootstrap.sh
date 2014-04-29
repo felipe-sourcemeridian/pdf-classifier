23a16472f479bba73441a35c0568d82a39082ac1
root <root@carlos-VirtualBox.(none)>
2014-04-29 17:46:03 -0500

    fixed package installation

--- interactive_classifier_bootstrap.sh
+++ interactive_classifier_bootstrap.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+sudo apt-get update
+sudo apt-get -q -y install  libglib2.0 unzip
+sudo apt-get -q -y install libicu48
 wget https://s3.amazonaws.com/classifier-process/data/interactive_classifier.tar.gz
 tar -xvzf interactive_classifier.tar.gz
+sudo dpkg -i classifier_0.1_amd64.deb
+sudo apt-get -q -y -f install
+
+set -e
 sudo ./install_classifier.sh $1
 rm -f interactive_classifier.tar.gz
 rm -f install_classifier.sh
